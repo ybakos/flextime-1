fd408e13008811927cebd117940b53eb03a4952a
Yong Bakos <ybakos@humanoriented.com>
2017-12-17 18:27:42 -0800

    Configure google oauth. - Config devise with google client id and secret. - Add google client id and secret to example env. - Create callbacks controller - Configure User model and define from_omniauth method. - Declare callback route. - Add provider and uid to User table. See https://github.com/zquestz/omniauth-google-oauth2 References #4.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # https://github.com/zquestz/omniauth-google-oauth2
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 end
