34690f521a6721170eff9bfbdc9de3cf7fb898cf
Yong Bakos <ybakos@humanoriented.com>
2017-12-19 07:43:44 -0800

    Teacher: Massage edit and delete experience. Extract form into partial, use dynamic submit label text. References #24.

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,30 +1,20 @@
 class TeachersController < ApplicationController
   before_action :set_teacher, only: [:show, :edit, :update, :destroy]
 
-  # GET /teachers
-  # GET /teachers.json
   def index
     @teacher = Teacher.new
     @teachers = Teacher.all
   end
 
-  # GET /teachers/1
-  # GET /teachers/1.json
-  def show
-  end
+  def show; end
 
-  # GET /teachers/1/edit
-  def edit
-  end
+  def edit; end
 
-  # POST /teachers
-  # POST /teachers.json
   def create
     @teacher = Teacher.new(teacher_params)
-
     respond_to do |format|
       if @teacher.save
-        format.html { redirect_to teachers_path, notice: 'Teacher was successfully created.' }
+        format.html { redirect_to teachers_path, notice: "#{@teacher} was successfully created." }
         format.json { render :show, status: :created, location: @teacher }
       else
         @teachers = Teacher.all
@@ -34,8 +24,6 @@ class TeachersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /teachers/1
-  # PATCH/PUT /teachers/1.json
   def update
     respond_to do |format|
       if @teacher.update(teacher_params)
@@ -48,24 +36,22 @@ class TeachersController < ApplicationController
     end
   end
 
-  # DELETE /teachers/1
-  # DELETE /teachers/1.json
   def destroy
     @teacher.destroy
     respond_to do |format|
-      format.html { redirect_to teachers_url, notice: 'Teacher was successfully destroyed.' }
+      format.html { redirect_to teachers_url, notice: "#{@teacher} was successfully removed." }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_teacher
       @teacher = Teacher.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def teacher_params
       params.require(:teacher).permit(:name, :title)
     end
+
 end
