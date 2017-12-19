672c82dc1784bd34c56ec83735e5f5f27508bd0b
Yong Bakos <ybakos@humanoriented.com>
2017-12-19 10:15:22 -0800

    Teacher: Test students cannot CRUD teachers (skip). References #24.

--- test/system/teachers_test.rb
+++ test/system/teachers_test.rb
@@ -2,6 +2,10 @@ require "application_system_test_case"
 
 class TeachersTest < ApplicationSystemTestCase
 
+  test 'students cannot CRUD teachers' do
+    skip
+  end
+
   test 'staff views a list of teachers' do
     visit teachers_url
     assert_selector 'h2', text: 'Teachers'
