981daa23a86d2aa3890c20d83be752d86357d008
Yong Bakos <ybakos@humanoriented.com>
2017-12-19 09:43:46 -0800

    Teacher: Test that the index displays a list of teachers. References #24.

--- test/system/teachers_test.rb
+++ test/system/teachers_test.rb
@@ -1,9 +1,11 @@
 require "application_system_test_case"
 
 class TeachersTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit teachers_url
-  #
-  #   assert_selector "h1", text: "Teacher"
-  # end
+
+  test 'staff views a list of teachers' do
+    visit teachers_url
+    assert_selector 'h2', text: 'Teachers'
+    assert_link 'Miss Valid'
+  end
+
 end
