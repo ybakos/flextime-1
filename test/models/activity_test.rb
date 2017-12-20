df610a30210c5abebf0eb57fd1f8d91424975e53
Yong Bakos <ybakos@humanoriented.com>
2017-12-19 20:18:32 -0800

    Activity: Test valid name, room and capacity. References #6, #22.

--- test/models/activity_test.rb
+++ test/models/activity_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class ActivityTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'without a name is invalid' do
+    activity = activities(:one)
+    assert activity.valid?
+    activity.name = nil
+    assert activity.invalid?
+  end
+
+  test 'without a room is invalid' do
+    activity = activities(:one)
+    assert activity.valid?
+    activity.room = nil
+    assert activity.invalid?
+  end
+
+  test 'without a capacity is invalid' do
+    activity = activities(:one)
+    assert activity.valid?
+    activity.capacity = nil
+    assert activity.invalid?
+  end
+
+  test 'capacity is an integer greater than or equal to 0' do
+    activity = activities(:one)
+    assert activity.valid?
+    activity.capacity = -1
+    assert activity.invalid?
+    activity.capacity = 2.3
+    assert activity.invalid?
+  end
+
 end
