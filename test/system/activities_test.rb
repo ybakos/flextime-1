6cdbf2c6dd816a56a73b011b448d3301250c044c
Yong Bakos <ybakos@humanoriented.com>
2017-12-25 16:33:45 -0700

    Activity: Refactor current week system test. Change Activity fixture titles to ensure uniqueness. References #22.

--- test/system/activities_test.rb
+++ test/system/activities_test.rb
@@ -7,32 +7,32 @@ class ActivitiesTest < ApplicationSystemTestCase
   end
 
   test 'staff views a list of activites for the current week' do
-    assert_selector 'h4', text: (Date.today.monday + 1).strftime("%B %-e")
-    assert_selector 'h5', text: 'Fake Tuesday Activity'
-    assert_selector 'h4', text: (Date.today.monday + 3).strftime("%B %-e")
-    assert_selector 'h5', text: 'Fake Thursday Activity'
-    assert_selector 'h4', text: (Date.today.monday + 4).strftime("%B %-e")
-    assert_selector 'h5', text: 'Fake Friday Activity'
+    assert_selector 'h2', text: 'This Week'
+    d = Date.today
+    [d.tuesday, d.thursday, d.friday].each do |day|
+      assert_selector 'h4', text: day.strftime("%B %-e")
+      assert_selector 'h5', text: "Fake #{day.day_name} Activity"
+    end
   end
 
   test 'staff views a list of activities for the previous week' do
     click_link 'Previous week'
     assert_selector 'h4', text: (Date.today.monday - 6).strftime("%B %-e")
-    assert_selector 'h5', text: 'Fake Tuesday Activity'
+    assert_selector 'h5', text: 'Fake Previous Tuesday Activity'
     assert_selector 'h4', text: (Date.today.monday - 4).strftime("%B %-e")
-    assert_selector 'h5', text: 'Fake Thursday Activity'
+    assert_selector 'h5', text: 'Fake Previous Thursday Activity'
     assert_selector 'h4', text: (Date.today.monday - 3).strftime("%B %-e")
-    assert_selector 'h5', text: 'Fake Friday Activity'
+    assert_selector 'h5', text: 'Fake Previous Friday Activity'
   end
 
   test 'staff views a list of activities for next week' do
     click_link 'Next week'
     assert_selector 'h4', text: (Date.today.monday + 8).strftime("%B %-e")
-    assert_selector 'h5', text: 'Fake Tuesday Activity'
+    assert_selector 'h5', text: 'Fake Next Tuesday Activity'
     assert_selector 'h4', text: (Date.today.monday + 10).strftime("%B %-e")
-    assert_selector 'h5', text: 'Fake Thursday Activity'
+    assert_selector 'h5', text: 'Fake Next Thursday Activity'
     assert_selector 'h4', text: (Date.today.monday + 11).strftime("%B %-e")
-    assert_selector 'h5', text: 'Fake Friday Activity'
+    assert_selector 'h5', text: 'Fake Next Friday Activity'
   end
 
   # https://github.com/osu-cascades/falcon-time/issues/30
