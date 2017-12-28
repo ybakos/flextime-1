fa78c3be8061a7fc5e85ad02cda1c80130364046
Yong Bakos <ybakos@humanoriented.com>
2017-12-27 21:22:24 -0700

    Fix sign in and out actions. Exempt the devise controller from the auth and student before actions. Set the sign out url. Get the sign in and out experience under test with new SessionsControllerTest.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :restrict_from_students
+  skip_before_action :authenticate_user!, if: :devise_controller?
+  skip_before_action :restrict_from_students, if: :devise_controller?
 
   private
 
@@ -12,6 +14,10 @@ class ApplicationController < ActionController::Base
 
     def after_sign_in_path_for(resource)
       current_user.student? ? student_path(current_user) : activities_path
+    end
+
+    def after_sign_out_path_for(resource)
+      new_user_session_path
     end
 
 end
---
+++ test/controllers/sessions_controller_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class SessionsControllerTest < ActionDispatch::IntegrationTest
+
+  include Devise::Test::IntegrationHelpers
+
+  test 'student signs in and out' do
+    student = users(:student)
+    sign_in student
+    get student_path(student)
+    assert_response :success
+    delete destroy_user_session_path
+    assert_redirected_to new_user_session_path
+  end
+
+  test 'staff signs in and out' do
+    staff = users(:staff)
+    sign_in staff
+    get activities_path
+    assert_response :success
+    delete destroy_user_session_path
+    assert_redirected_to new_user_session_path
+  end
+
+end
