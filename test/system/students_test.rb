2e60d1f3607d5c4e3b2857e877a34efaa7575383
Yong Bakos <ybakos@humanoriented.com>
2017-12-29 13:25:13 -0700

    Student: Refactor and stub system tests.

--- test/system/students_test.rb
+++ test/system/students_test.rb
@@ -4,11 +4,34 @@ class StudentsTest < ApplicationSystemTestCase
 
   include Devise::Test::IntegrationHelpers
 
+  def sign_in_as_student_and_visit_profile
+    sign_in users(:student)
+    visit student_path(users(:student))
+  end
+
   test 'student views their own profile' do
-    student = users(:student)
-    sign_in student
-    visit student_path(student)
-    assert_selector 'h2', text: student.to_s
+    sign_in_as_student_and_visit_profile
+    assert_selector 'h2', text: users(:student).to_s
+  end
+
+  test 'student views their Falcon Time for the current week' do
+    sign_in_as_student_and_visit_profile
+    skip
+  end
+
+  test 'student views their Falcon Time for the previous week' do
+    sign_in_as_student_and_visit_profile
+    skip
+  end
+
+  test 'student views their Falcon Time for the next week' do
+    sign_in_as_student_and_visit_profile
+    skip
+  end
+
+  test 'student specifies a teacher' do
+    sign_in_as_student_and_visit_profile
+    skip
   end
 
 end
