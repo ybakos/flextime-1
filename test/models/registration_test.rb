64d221522611875530228df11207d2c2755f3658
Yong Bakos <ybakos@humanoriented.com>
2017-12-30 13:32:17 -0800

    Registration: Test association methods. References #1, #2, #8, #9, #10, #13, #14, #31, #32.

--- test/models/registration_test.rb
+++ test/models/registration_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class RegistrationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'has a creator that is a User' do
+    assert_respond_to registrations(:by_student), :creator
+    assert_kind_of User, registrations(:by_student).creator
+  end
+
+  test 'has a student that is a User' do
+    assert_respond_to registrations(:by_student), :student
+    assert_kind_of User, registrations(:by_student).student
+  end
+
+  test 'has a teacher' do
+    assert_respond_to registrations(:by_student), :teacher
+    assert_kind_of Teacher, registrations(:by_student).teacher
+  end
+
+  test 'has an activity' do
+    assert_respond_to registrations(:by_student), :activity
+    assert_kind_of Activity, registrations(:by_student).activity
+  end
+
 end
