a6e26977320777b66323d77a121b6c232564323f
Yong Bakos <ybakos@humanoriented.com>
2018-01-07 22:00:04 -0800

    Student: Enable previous/next week buttons. Duplicate the helper from activities, to leave a smell for later refactoring. References #21.

--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -1,2 +1,11 @@
 module StudentsHelper
+
+  def link_to_previous_week_for_student(student, date)
+    link_to 'Previous week', student_path(student, date: date.prev_week), class: 'btn btn-outline-secondary'
+  end
+
+  def link_to_next_week_for_student(student, date)
+    link_to 'Next week', student_path(student, date: date.next_week), class: 'btn btn-outline-primary float-right'
+  end
+
 end
