9ec0582855aea5aceaa515bca7cd38a2aeb4d967
Yong Bakos <ybakos@humanoriented.com>
2018-04-16 07:44:22 -0700

    students/show: Show teacher instead of form, and improve UX. If the student has a teacher already, display the teacher and not the form. (So students don't arbitrarily change their FT teacher.)
    If the student does not have a teacher, display a prompt to select the
    teacher instead of the current week schedule.
    References #38, #49.

--- test/system/students_test.rb
+++ test/system/students_test.rb
@@ -36,13 +36,14 @@ class StudentsTest < ApplicationSystemTestCase
 
   test 'student sees current Falcon Time teacher' do
     sign_in_as_student_and_visit_profile
-    assert has_select?('student_teacher_id', selected: users(:student).teacher.to_s)
+    assert_selector('h3#falcon_time_teacher', text: users(:student).teacher.to_s)
   end
 
   # Specifying teacher
 
   test 'student specifies a teacher' do
-    sign_in_as_student_and_visit_profile
+    sign_in users(:new_student_without_teacher)
+    visit student_path(users(:new_student_without_teacher))
     select 'Mr. Valid', from: 'student_teacher_id'
     click_button 'Ok'
     assert_text 'Falcon Time teacher set to Mr. Valid'
@@ -50,11 +51,12 @@ class StudentsTest < ApplicationSystemTestCase
   end
 
   test 'student sees an error when specifying no teacher' do
-    sign_in_as_student_and_visit_profile
+    sign_in users(:new_student_without_teacher)
+    visit student_path(users(:new_student_without_teacher))
     select 'Choose...', from: 'student_teacher_id'
     click_button 'Ok'
     assert_text 'Please specify a teacher'
-    assert has_select?('student_teacher_id', selected: users(:student).teacher.to_s)
+    assert has_select?('student_teacher_id')
   end
 
 end
