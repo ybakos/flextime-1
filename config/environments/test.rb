77c0dade1699862d64594d6a53e476f6398b939f
Yong Bakos <ybakos@humanoriented.com>
2018-08-10 19:12:13 -0700

    MAJOR: Upgrade Rails from 5.1.6 to 5.2.1.
    Update the rails version in the Gemfile.
    Run rails app:update.
    Add changes to misc files per rails diff. (See http://railsdiff.org/5.1.6/5.2.1)

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
 
   config.public_file_server.enabled = true
   config.public_file_server.headers = {
-    'Cache-Control' => "public, max-age=#{1.hour.seconds.to_i}"
+    'Cache-Control' => "public, max-age=#{1.hour.to_i}"
   }
 
   config.consider_all_requests_local       = true
@@ -15,6 +15,8 @@ Rails.application.configure do
   config.action_dispatch.show_exceptions = false
 
   config.action_controller.allow_forgery_protection = false
+
+  config.active_storage.service = :test
 
   config.action_mailer.perform_caching = false
 
