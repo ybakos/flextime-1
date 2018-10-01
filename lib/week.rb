c9c7d091bcc25e3753265c106271fc9e635b8b9d
Yong Bakos <ybakos@humanoriented.com>
2018-09-30 23:55:45 -0700

    Week: Fix spelling of wednesday.

--- lib/week.rb
+++ lib/week.rb
@@ -1,6 +1,6 @@
 class Week
 
-  DAYS = [:monday, :tuesday, :wedneday, :thursday, :friday]
+  DAYS = [:monday, :tuesday, :wednesday, :thursday, :friday]
   # ACTIVITY_DAYS = ...see config/initializers/week.rb
 
   def self.to_string_array
