284927c3965bb8b2b6388eda67ccb34fa2ab4be0
Yong Joseph Bakos <ybakos@humanoriented.com>
2019-10-26 14:59:03 -0700

    Devise: Extract sender email config into env var.
    References #111.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,7 +10,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'no-reply@falcontime.net'
+  config.mailer_sender = ENV.fetch('DEVISE_MAILER_SENDER') { 'no-reply@example.com' }
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
