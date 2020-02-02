ae2e2414bb72fafaa6d2c6d1422274544847be2b
Dylan Drudge <drudged@oregonstate.edu>
2020-02-02 13:53:51 -0800

    add visibility intop n+1 queries

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'guard'
   gem 'guard-minitest'
+  gem 'bullet'
 end
 
 group :test do
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,4 +33,11 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.alert = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+  end
 end
