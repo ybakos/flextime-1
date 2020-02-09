51964a6b8b2dfb0d04ce20f9d836f483917d7884
Dylan Drudge <drudged@oregonstate.edu>
2020-02-09 14:01:48 -0800

    add bullet and dev config for bullet logging

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,4 +33,10 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  #bullet optimization logging config
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.alert = true
+    Bullet.bullet_logger = true
+  end
 end
