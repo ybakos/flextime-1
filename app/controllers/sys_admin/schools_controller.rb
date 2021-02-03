66b9cebd7376b6d60483c035f7a9733f38c50c7f
Yong Bakos <ybakos@humanoriented.com>
2021-02-02 18:31:00 -0800

    SysAdmin/Schools: Add new and create.
    References #153.

--- app/controllers/sys_admin/schools_controller.rb
+++ app/controllers/sys_admin/schools_controller.rb
@@ -8,10 +8,27 @@ class SysAdmin::SchoolsController < SysAdmin::SysAdminController
     @school = School.find(params[:id])
   end
 
-  def new; end
-  def create; end
+  def new
+    @school = School.new
+  end
+
+  def create
+    @school = School.new(school_params)
+    if @school.save
+      redirect_to sys_admin_schools_url, notice: "School #{@school} has been created."
+    else
+      render :new
+    end
+  end
+
   def edit; end
   def update; end
   def destroy; end
+
+  private
+
+    def school_params
+      params.require(:school).permit(:name, :slug)
+    end
 
 end
--- test/system/sys_admin/schools_test.rb
+++ test/system/sys_admin/schools_test.rb
@@ -18,4 +18,20 @@ class SysAdminViewsSchoolsTest < ApplicationSystemTestCase
     assert_text school.name
   end
 
+  test 'sys_admin creates a new school with invalid attributes' do
+    sign_in(users(:sys_admin))
+    visit new_sys_admin_school_path
+    click_on 'Create School'
+    assert_text 'error'
+  end
+
+  test 'sys_admin creates a new school with valid attributes' do
+    sign_in(users(:sys_admin))
+    visit new_sys_admin_school_path
+    fill_in 'Name', with: 'Fake School Name'
+    fill_in 'Slug', with: 'fakeschoolslug'
+    click_on 'Create School'
+    assert_text 'has been created'
+  end
+
 end
