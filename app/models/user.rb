ybakos/flextime-1

commit c7837096ff3832e9c4b68cdf2fc9e765aca331b4

User: Add nil check for last name.
It seems that in some cases, the google auth info may not contain a
last name. When it is nil, capitalization fails. Add a nil check, and use
the empty string when blank.

References #182.

app/models/user.rb
@@ -34,7 +34,7 @@ class User < ApplicationRecord
       user.password = Devise.friendly_token[0,20]
       user.email = auth.info.email
       user.first_name = auth.info.first_name.capitalize
-      user.last_name = auth.info.last_name.capitalize
+      user.last_name = auth.info.last_name&.capitalize || ''
       user.image_url = auth.info.image
     end
   end

commit d758d7065db28e2b4dc357191b700c156f62cdca

User: Use placeholder string for unknown last name.
Commit c7837096ff3832e9c4b68cdf2fc9e765aca331b4 introduced a bug:
it was using an empty string when the last name wasn't in the auth
payload, and last names can't be blank.

This causes first_or_create to return an unsaved user (validations fail)
which then results in the auth controller to send a redirect and deny
the sign in attempt.

Replace the empty string with 'Unknown'.

References #182

app/models/user.rb
@@ -34,7 +34,7 @@ class User < ApplicationRecord
       user.password = Devise.friendly_token[0,20]
       user.email = auth.info.email
       user.first_name = auth.info.first_name.capitalize
-      user.last_name = auth.info.last_name&.capitalize || ''
+      user.last_name = auth.info.last_name&.capitalize || '(Unknown)'
       user.image_url = auth.info.image
     end
   end